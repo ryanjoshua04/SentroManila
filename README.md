a085b253c54e5c2d418aa537c60d173c8f72ae30
Ryan <ryanjoshuajimenez@gmail.com>
2021-11-08 13:21:21 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sentro Manila Project
---
+++ shopsite/views.py
@@ -0,0 +1,9 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request,'home.html');
+
+def order(request):
+    return render(request,'makeorder.html');
