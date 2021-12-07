6e83249b8b8df15a904a55b7d62b00ba2368f56f
Ryan <ryanjoshuajimenez@gmail.com>
2021-12-07 19:49:06 +0800

    OTP Feature Added

--- shopsite/urls.py
+++ shopsite/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('order/<int:id>/',views.order, name='order'),
-    path('makeorder/<int:id>/<str:name>',views.makeorder, name='makeorder')
+    path('makeorder/<int:id>/<str:name>',views.makeorder, name='makeorder'),
+    path('otp_confirmation/<int:id>/<str:name>', views.otp_confirmation, name='otp_confirmation')
 ]
