0768aaa9500da24e5bfaa8ae49bf5d07f8f834bb
Ryan <ryanjoshuajimenez@gmail.com>
2021-12-10 05:13:08 +0800

    otp changes

--- shopsite/views.py
+++ shopsite/views.py
@@ -89,7 +89,7 @@ def otp_confirmation(request, id, name):
     back = "/order/{}".format(id)
     if request.method == 'POST':
         customerinput = request.POST['otp_confirm']
-        checkotp = OTPs.objects.get(otpcurrent=customerinput).exists()
+        checkotp = OTPs.objects.filter(otpcurrent=customerinput).exists()
         if checkotp == True:
             firstname = first()
             lastname = last()
@@ -114,7 +114,7 @@ def otp_confirmation(request, id, name):
         else:
             checkexpire = OTPs.objects.filter(otp_expire=datetime.now()).exist
             if checkexpire == False:
-                OTPs.objects.filter(otp_expire__lte=datetime.now()-timedelta(seconds=10)).delete()
+                OTPs.objects.filter(otp_expire__lte=datetime.now()-timedelta(seconds=60)).delete()
                 messages.error(request, 'Uh-oh, You have entered an invalid OTP. Please Try to order again to generate new OTP')
                 return redirect(back)
             else:
